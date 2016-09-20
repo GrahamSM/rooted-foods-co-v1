991e77350373a9feb28a80d6178a6c6054d23ef7
graham <graham.sm94@gmail.com>
2016-09-20 00:22:18 -0400

    Added login, signup, and cart buttons and implemented routing for login and signup pages

--- react-redux-starter/src/client/components/Header/RightHeader/RightThird/RightThird.js
+++ react-redux-starter/src/client/components/Header/RightHeader/RightThird/RightThird.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import styles from './RightThird.scss';
 
 //components
+import Login from './Login.js';
+import SignUp from './SignUp.js';
+import Cart from './Cart.js';
 
 // var Reqwest = require('reqwest');
 export default class RightThird extends React.Component{
@@ -12,7 +15,14 @@ export default class RightThird extends React.Component{
   render(){
     // TODO: Give each FilterBtn a click handler that renders relevant data
     return(
-      <div>
+      <div className='right-third-header'>
+        <div className='user-logsign'>
+          <Login/>
+          <SignUp/>
+        </div>
+        <div className='cart'>
+          <Cart/>
+        </div>
       </div>
     )
   }
--- react-redux-starter/src/index.js
+++ react-redux-starter/src/index.js
@@ -4,7 +4,10 @@ import { render } from 'react-dom';
 
 // Import components
 import App from './client/components/App';
-import PageBody from './client/components/PageBody/PageBody';
+import PageBody from './client/components/PageBody/PageBody.js';
+import LoginPage from './client/components/LoginSignUp/LoginPage.js';
+import SignUpPage from './client/components/LoginSignUp/SignUpPage.js';
+
 
 // Import react router deps
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
@@ -16,6 +19,8 @@ const router = (
     <Router history={history}>
       <Route path='/' component={App}>
         <IndexRoute component={PageBody}></IndexRoute>
+        <Route path='/login' component={LoginPage}></Route>
+        <Route path='/signup' component={SignUpPage}></Route>
       </Route>
     </Router>
   </Provider>
